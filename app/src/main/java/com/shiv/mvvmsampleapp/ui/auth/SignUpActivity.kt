19df5965bd32ccdbc0a0f100d92881ab683e8080
shivanshumishra <shivanshufeb14@gmail.com>
2023-06-25 15:33:14 +0530

    UI design for login and signup

--- app/src/main/java/com/shiv/mvvmsampleapp/ui/auth/SignUpActivity.kt
+++ app/src/main/java/com/shiv/mvvmsampleapp/ui/auth/SignUpActivity.kt
@@ -1,11 +1,12 @@
-package com.shiv.mvvmsampleapp
+package com.shiv.mvvmsampleapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shiv.mvvmsampleapp.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
